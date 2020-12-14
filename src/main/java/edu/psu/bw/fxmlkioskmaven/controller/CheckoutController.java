833c615170022e9f3e0a7c78e723b6861b212fca
unknown <mcmanusnj3@gmail.com>
2020-12-14 05:03:07 -0500

    Total calculation works within the checkout call

--- src/main/java/edu/psu/bw/fxmlkioskmaven/controller/CheckoutController.java
+++ src/main/java/edu/psu/bw/fxmlkioskmaven/controller/CheckoutController.java
@@ -12,6 +12,8 @@ import edu.psu.bw.fxmlkioskmaven.model.CartItem;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -24,7 +26,9 @@ import javafx.fxml.Initializable;
  */
 public class CheckoutController implements Initializable {
 
+    
     private ObservableList<CartItem> cart;
+    private DoubleProperty total;
     
     /**
      * Initializes the controller class.
@@ -32,12 +36,23 @@ public class CheckoutController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         cart = new SimpleListProperty(FXCollections.observableArrayList());
+        total = new SimpleDoubleProperty(0);
     }    
     
     public void loadCart(List<CartItem> cart)
     {
         this.cart.addAll(cart);
-        System.out.println("Cart passed into Checkout:");
-        cart.forEach(e -> {System.out.println(e);});
+        System.out.println("Cart passed into Checkout: Size: " + cart.size());
+        cart.forEach(item -> 
+        {
+            System.out.println("Name: " + item.getItem().getName());
+            addItemCost(item);
+        });
+        System.out.println("Total: " + String.format("$%.2f", this.total.getValue()));
+    }
+    
+    private void addItemCost(CartItem item)
+    {
+        total.setValue(total.add(item.total().get()).get());
     }
 }
